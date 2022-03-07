d12e457459b196d45a9c704d21250c15290255b4
name <email>
2022-03-07 13:09:29 +0000

    Commit from 2022-03-07 13:09:29

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#testovaci commit a push 46
+#testovaci commit a push 47
 
 # Minimalistický lokální katalog otevřených dat
 Toto je minimalistická verze lokálního katalogu otevřených dat, která celá běží na GitHubu, a tedy zdarma.
--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 if [[ $(git config --local --get user.name) ]]; then
   echo "je nastavenej"
+  echo "odnastavuji"
+  git config --local --unset user.name
 else
   echo "Neni nastavenej"
+  echo "Nastavuji"
+  git config --local user.name "Testovaci jmeno1"
 fi
